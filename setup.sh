fdbbde24a3067af7d15c1ab123ab8d59e11e2acd
Jonathan Kans <kans@ncbi.nlm.nih.gov>
2014-08-30 00:00:05 +0000

    setup script now configures PATH with current path, even if not HOME/edirect
    git-svn-id: https://svn.ncbi.nlm.nih.gov/repos/toolkit/trunk/internal/scripts/edirect@445087 470b1a5d-c923-0410-adfa-dd7f1cf1b600

--- setup.sh
+++ setup.sh
@@ -1,34 +1,34 @@
 #!/bin/bash -norc
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 if [ ! -e "$HOME/.bashrc" ]
 then
   echo 'source ~/.bash_profile' > $HOME/.bashrc
 fi
 if [ ! -e "$HOME/.bash_profile" ]
 then
-  echo 'export PATH=$PATH:$HOME/edirect' > $HOME/.bash_profile
+  echo "export PATH=\$PATH:$DIR" > $HOME/.bash_profile
 fi
+
 if ! grep 'bash_profile' "$HOME/.bashrc" >/dev/null 2>&1
 then
   echo 'source ~/.bash_profile' >> $HOME/.bashrc
 fi
-if ! grep '\$HOME/edirect' "$HOME/.bash_profile" >/dev/null 2>&1
+if ! grep "PATH.*edirect" "$HOME/.bash_profile" >/dev/null 2>&1
 then
-  echo 'export PATH=$PATH:$HOME/edirect' >> $HOME/.bash_profile
+  echo "export PATH=\$PATH:$DIR" >> $HOME/.bash_profile
 fi
+
 echo ""
 echo "Welcome to Entrez Direct"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ "$DIR" != "$HOME/edirect" ]
-then
 echo ""
-echo "The Entrez Direct scripts appear to be located in a non-standard area."
+echo "Please close this terminal window and open a new one to start working with Entrez Direct."
 echo ""
-echo "You may need to add the following command to the .bash_profile configuration file:"
+echo "If the 'esearch -version' command reports \"command not found\", you may need to add the"
+echo "following line to the (invisible) .bash_profile configuration file in your home directory:"
 echo ""
 echo "  export PATH=\$PATH:$DIR"
 echo ""
-echo "in order to run programs without needing to prefix them with \"\./\"."
-else
+echo "editing it if necessary to indicate the exact path to the edirect directory."
 echo ""
-echo "Please close this terminal window and open a new one to start working with Entrez Direct."
-fi
